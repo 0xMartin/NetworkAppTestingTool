3c5b04f84e34632cb91c986de6b63aba72331a35
Martin Krčma <58054719+0xMartin@users.noreply.github.com>
2024-08-08 15:53:16 +0200

    Delete config-editor directory

--- config-editor/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-python main.py
-if %ERRORLEVEL% neq 0 (
-    echo Python not found. Make sure you have Python installed and set correctly in PATH.
-    pause
-)
--- config-editor/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! command -v python3 &> /dev/null
-then
-    echo "Python3 is not installed. Please install Python3."
-    exit 1
-fi
-
-python3 main.py
