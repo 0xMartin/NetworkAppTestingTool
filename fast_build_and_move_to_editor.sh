0f64dcaa04f55a3c58840373f104b80d426bdfff
0xMartin <martin.krcma1@gmail.com>
2024-08-11 16:59:44 +0200

    build scripts update

--- fast_build_and_move_to_editor.sh
+++ fast_build_and_move_to_editor.sh
@@ -2,5 +2,6 @@
 
 ./gradlew :natt:build -x test
 
-cp app/build/libs/app-all.jar natt-config-editor/NATT.jar && echo "NATT.jar moved to config-editor"
+cp natt/build/libs/natt-*-all.jar natt-config-editor/NATT.jar && echo "NATT.jar moved to config-editor"
+cp natt/build/libs/natt-*-all.jar examples/project-example/NATT.jar && echo "NATT.jar moved to project-example"
 
---
+++ full_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+./gradlew :natt-spi:build
+
+cp natt-spi/build/libs/natt-spi-*.jar natt/libs && echo "Copied natt-spi to natt/libs"
+
+cp natt-spi/build/libs/natt-spi-*.jar examples/plugin-example/plugin/libs && echo "Copied natt-spi to examples/plugin-example/plugin/libs"
+
+./gradlew :natt:build
+
+cp natt/build/libs/natt-*-all.jar natt-config-editor/NATT.jar && echo "NATT.jar moved to config-editor"
+cp natt/build/libs/natt-*-all.jar examples/project-example/NATT.jar && echo "NATT.jar moved to project-example"
